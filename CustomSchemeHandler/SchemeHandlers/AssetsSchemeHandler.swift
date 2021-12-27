b320ba14b5926086b4562f824f4346958ce76a49
Gualtiero <gualtiero.frigerio@gmail.com>
2021-12-27 12:09:29 +0100

    added AssetSchemeHandler

---
+++ CustomSchemeHandler/SchemeHandlers/AssetsSchemeHandler.swift
@@ -0,0 +1,45 @@
+//
+//  AssetsSchemeHandler.swift
+//  CustomSchemeHandler
+//
+//  Created by Gualtiero Frigerio on 24/12/21.
+//
+
+import Foundation
+import UniformTypeIdentifiers
+import WebKit
+
+class AssetsSchemeHandler: NSObject, SchemeHandler {
+    func webView(_ webView: WKWebView, start urlSchemeTask: WKURLSchemeTask) {
+        guard let url = urlSchemeTask.request.url,
+              let fileUrl = fileUrlFromUrl(url),
+              let mimeType = mimeType(ofFileAtUrl: fileUrl),
+              let data = try? Data(contentsOf: url) else { return }
+       
+        let response = HTTPURLResponse(url: url,
+                                       mimeType: mimeType,
+                                       expectedContentLength: data.count, textEncodingName: nil)
+        
+        urlSchemeTask.didReceive(response)
+        urlSchemeTask.didReceive(data)
+        urlSchemeTask.didFinish()
+    }
+    
+    func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) {
+        
+    }
+    
+    // MARK: - Private
+    
+    private func fileUrlFromUrl(_ url: URL) -> URL? {
+        Bundle.main.url(forResource: url.absoluteString,
+                        withExtension: "")
+    }
+    
+    private func mimeType(ofFileAtUrl url: URL) -> String? {
+        guard let type = UTType(filenameExtension: url.pathExtension) else {
+            return nil
+        }
+        return type.preferredMIMEType
+    }
+}
