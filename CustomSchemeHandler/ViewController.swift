0628fe9e0c4f4531e4b652ef43b1a68d3535f54c
Gualtiero <gualtiero.frigerio@gmail.com>
2021-12-28 17:28:57 +0100

    refactoring

--- CustomSchemeHandler/ViewController.swift
+++ CustomSchemeHandler/ViewController.swift
@@ -11,9 +11,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupHandlers()
+        createWebView()
     }
+    
+    private var handlers: [SchemeHandlerEntry] = []
 
-
+    private func createWebView() {
+        let handler = WebViewHandler(withSchemeHandlers: handlers)
+        view.addSubview(handler.webView)
+        handler.webView.frame = view.frame
+        
+        guard let testURL = Bundle.main.url(forResource: "test",
+                                            withExtension: "html",
+                                            subdirectory: "assets") else { return }
+        handler.loadRequest(URLRequest(url: testURL))
+    }
+    
+    private func setupHandlers() {
+        if let databasePath = Bundle.main.path(forResource: "db",
+                                               ofType: "sqlite",
+                                               inDirectory: "assets") {
+            let schemeHandler = SchemeHandlerFactory.createWithType(.sqlite(databasePath: databasePath))
+            let entry = SchemeHandlerEntry(schemeHandler: schemeHandler,
+                                           urlScheme: "sqlite")
+            handlers.append(entry)
+        }
+        
+        let assetsHandler = AssetsSchemeHandler()
+        let entry = SchemeHandlerEntry(schemeHandler: assetsHandler, urlScheme: "assets")
+        handlers.append(entry)
+    }
 }
 
