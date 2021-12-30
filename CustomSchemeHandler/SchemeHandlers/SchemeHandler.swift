c6669afe7237578119ee5ecf11ec3ac828792c3b
Gualtiero <gualtiero.frigerio@gmail.com>
2021-12-30 10:51:35 +0100

    added responseError

--- CustomSchemeHandler/SchemeHandlers/SchemeHandler.swift
+++ CustomSchemeHandler/SchemeHandlers/SchemeHandler.swift
@@ -17,6 +17,19 @@ protocol SchemeHandler: WKURLSchemeHandler {
     
 }
 
+/// extension to add a default HTTPURLResponse in case of error to be used by concrete types
+extension SchemeHandler {
+    static func responseError(forUrl url: URL?, message: String) -> (HTTPURLResponse, Data) {
+        let responseUrl = url ?? URL(string: "error://error")!
+        let response = HTTPURLResponse(url: responseUrl,
+                                       mimeType: nil,
+                                       expectedContentLength: message.count,
+                                       textEncodingName: "utf-8")
+        let data = message.data(using: .utf8) ?? Data()
+        return (response, data)
+    }
+}
+
 class SchemeHandlerFactory {
     static func createWithType(_ type: SchemeHandlerType) -> SchemeHandler {
         switch type {
