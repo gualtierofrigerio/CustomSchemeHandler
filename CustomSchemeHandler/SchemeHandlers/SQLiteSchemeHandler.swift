2af1f85007c94ca7826a3838e90823a86115e32c
Gualtiero <gualtiero.frigerio@gmail.com>
2021-12-30 10:52:11 +0100

    refactoring

--- CustomSchemeHandler/SchemeHandlers/SQLiteSchemeHandler.swift
+++ CustomSchemeHandler/SchemeHandlers/SQLiteSchemeHandler.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-
 import WebKit
 
 class SQLiteSchemeHandler: NSObject, SchemeHandler {
@@ -15,12 +14,7 @@ class SQLiteSchemeHandler: NSObject, SchemeHandler {
     }
     
     func webView(_ webView: WKWebView, start urlSchemeTask: WKURLSchemeTask) {
-        guard let url = urlSchemeTask.request.url else { return }
-        let data = dataForURL(url)
-        let response = HTTPURLResponse(url: url,
-                                       mimeType: "application/json",
-                                       expectedContentLength: data?.count ?? 0,
-                                       textEncodingName: "utf-8")
+        let (response, data) = responseAndData(fromRequest: urlSchemeTask.request)
         
         urlSchemeTask.didReceive(response)
         if let data = data {
@@ -32,50 +26,51 @@ class SQLiteSchemeHandler: NSObject, SchemeHandler {
     func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) {
         
     }
-}
+    
+    // MARK: - Private
 
-// MARK: - Private
+    private var sqlWrapper: SQLiteWrapper
 
-private var sqlWrapper: SQLiteWrapper?
-
-private func dataForURL_noquery(_ url: URL) -> Data? {
-    var data: Data?
-    guard let wrapper = sqlWrapper,
-          let urlValue = url.host,
-          let jsonObject = wrapper.performQuery("select * from \(urlValue)") else { return nil }
-    if JSONSerialization.isValidJSONObject(jsonObject) {
-        data = try? JSONSerialization.data(withJSONObject: jsonObject,
-                                           options: .fragmentsAllowed)
+    private func dataForURL_noquery(_ url: URL) -> Data? {
+        guard let urlValue = url.host,
+              let jsonObject = sqlWrapper.performQuery("select * from \(urlValue)") else { return nil }
+        return JSONSerialization.data(fromJSON: jsonObject)
     }
-    return data
-}
 
-private func dataForURL(_ url: URL) -> Data? {
-    var data: Data?
-    guard let wrapper = sqlWrapper,
-          let host = url.host else { return nil }
-    let queryItems = URLComponents(string: url.absoluteString)?.queryItems ?? []
-    var query: String = ""
-    if queryItems.count > 0 {
-        var whereStr = ""
-        for item in queryItems {
-            if let value = item.value {
-                if whereStr != "" {
-                    whereStr += " AND "
-                }
-                whereStr += "\(item.name) = \(value)"
-            }
-        }
-        query = "select * from \(host) WHERE \(whereStr)"
-    }
-    else {
-        query = "select * from \(host)"
+    private func dataForURL(_ url: URL) -> Data? {
+        guard let query = SQLQueryHelper.makeQuery(fromURL: url),
+              let jsonObject = sqlWrapper.performQuery(query) else { return nil }
+        return JSONSerialization.data(fromJSON: jsonObject)
     }
     
-    guard let jsonObject = wrapper.performQuery(query) else { return nil }
-    if JSONSerialization.isValidJSONObject(jsonObject) {
-        data = try? JSONSerialization.data(withJSONObject: jsonObject,
-                                           options: .fragmentsAllowed)
+    private func responseAndData(fromRequest request: URLRequest) -> (URLResponse, Data?) {
+        guard let url = request.url,
+              let method = request.httpMethod else {
+                  return Self.responseError(forUrl: request.url,
+                                               message: "Error while parsing request")
+              }
+        if method == "POST" {
+            return responseFromPOST(url: url, data: request.httpBody)
+        }
+        else {
+            let data = dataForURL(url)
+            let response = HTTPURLResponse.jsonResponse(url: url, data: data)
+            return (response, data)
+        }
     }
-    return data
+    
+    private func responseFromPOST(url: URL, data: Data?) -> (URLResponse, Data?) {
+        guard let insertQuery = SQLQueryHelper.makeInsert(url: url, data: data) else {
+            return Self.responseError(forUrl: url,
+                                         message: "Error while parsing request")
+        }
+        guard let jsonObject = sqlWrapper.performQuery(insertQuery) else {
+            return Self.responseError(forUrl: url, message: "error while inserting data")
+        }
+        let returnData = JSONSerialization.data(fromJSON: jsonObject)
+        let response = HTTPURLResponse.jsonResponse(url: url, data: data)
+        return (response, returnData)
+    }
 }
+
+
