de742953969e7defe6c68deabe6bbe639fcaef45
Gualtiero <gualtiero.frigerio@gmail.com>
2022-01-07 10:44:08 +0100

    added new files for test

---
+++ CustomSchemeHandlerTests/WKRULSchemeTaskTest.swift
@@ -0,0 +1,52 @@
+//
+//  WKRULSchemeTaskTest.swift
+//  CustomSchemeHandlerTests
+//
+//  Created by Gualtiero Frigerio on 06/01/22.
+//
+
+import Foundation
+import WebKit
+
+class WKURLSchemeTaskTest: NSObject {
+    private (set) var request: URLRequest
+    
+    init(withRequest: URLRequest) {
+        self.request = withRequest
+    }
+    
+    func expectData(data: Data, completionHandler: @escaping (Bool) -> Void) {
+        self.expectedData = data
+        self.completionHandler = completionHandler
+    }
+    
+    private var completionHandler: ((Bool) -> Void)?
+    private var expectedData: Data?
+    private var receivedData: Data?
+}
+
+extension WKURLSchemeTaskTest: WKURLSchemeTask {
+    func didReceive(_ response: URLResponse) {
+        
+    }
+    
+    func didReceive(_ data: Data) {
+        if receivedData == nil {
+            receivedData = Data()
+        }
+        receivedData?.append(data)
+    }
+    
+    func didFinish() {
+        if receivedData == expectedData {
+            completionHandler?(true)
+        }
+        else {
+            completionHandler?(false)
+        }
+    }
+    
+    func didFailWithError(_ error: Error) {
+        completionHandler?(false)
+    }
+}
